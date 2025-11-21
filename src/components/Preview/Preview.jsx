bc6f38f40d7db72742286563e3b1e2d3b5b0bebd
Shayanin Vanichrudee <shayanin.van@gmail.com>
2025-11-21 09:42:10 +0700

    add markdown support

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -1,17 +1,24 @@
 import styles from "./Preview.module.css";
-import { useEffect } from "react";
 import { splitByJson } from "../../tools/utils/splitByJson";
 import Diagram from "./Diagram/Diagram";
+import Markdown from "react-markdown";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import "katex/dist/katex.min.css";
 
 function Preview({ problem }) {
-  useEffect(() => {
-    MathJax.typeset();
-  }, [problem]);
-
   const splittedQuestion = splitByJson(problem.question);
   const question = splittedQuestion.map((item, index) => {
     if (item.type === "text") {
-      return <p key={index}>{item.text}</p>;
+      return (
+        <Markdown
+          remarkPlugins={[remarkMath]}
+          rehypePlugins={[rehypeKatex]}
+          key={index}
+        >
+          {item.text}
+        </Markdown>
+      );
     } else if (item.type === "jsonObj") {
       return <Diagram key={index} objJson={item.jsonObj}></Diagram>;
     }
@@ -19,10 +26,22 @@ function Preview({ problem }) {
 
   const choices = [];
   for (let i = 0; i < problem.choices.length; i++) {
-    choices.push(<p key={i}>{i + 1 + ". " + problem.choices[i]}</p>);
+    choices.push(
+      <Markdown
+        remarkPlugins={[remarkMath]}
+        rehypePlugins={[rehypeKatex]}
+        key={i}
+      >
+        {i + 1 + ". " + problem.choices[i]}
+      </Markdown>
+    );
   }
 
-  const hint = <p>{"คำใบ้ : " + problem.hint}</p>;
+  const hint = (
+    <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+      {"คำใบ้ : " + problem.hint}
+    </Markdown>
+  );
 
   const answerNum = problem.answer + 1;
   const answer = <p>{"เฉลยหยาบ : " + answerNum}</p>;
@@ -30,7 +49,15 @@ function Preview({ problem }) {
   const splittedSolution = splitByJson(problem.solution);
   const solution = splittedSolution.map((item, index) => {
     if (item.type === "text") {
-      return <p key={index}>{item.text}</p>;
+      return (
+        <Markdown
+          remarkPlugins={[remarkMath]}
+          rehypePlugins={[rehypeKatex]}
+          key={index}
+        >
+          {item.text}
+        </Markdown>
+      );
     } else if (item.type === "jsonObj") {
       return <Diagram key={index} objJson={item.jsonObj}></Diagram>;
     }
